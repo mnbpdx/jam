748ce7b1b1c51809665e63caa157b5e6f33ba45e
Mark Beebe <mbeebe08@gmail.com>
2022-06-17 17:29:53 -0700

    fix: adjust padding a bit, add a long fake

--- app/src/main/java/com/mb/jam/model/Episode.kt
+++ app/src/main/java/com/mb/jam/model/Episode.kt
@@ -1,7 +1,5 @@
 package com.mb.jam.model
 
-import java.util.Date
-
 data class Episode(
     val title: String,
     val episodeNumber: String, // needs better name. means "S1 E20" or "BONUS"
@@ -18,4 +16,14 @@ fun getFakeEpisode() = Episode(
     releaseDate = "JULY 14",
     isDownloaded = false,
     isPlayed = false
-    )
+)
+
+fun getExtraLongFakeEpisode() = Episode(
+    title = "The Whole Mufulata And This Is A Really Long Title That Keeps Going And " +
+        "Going And Going And Going And Going And This is The end.",
+    episodeNumber = "S5 E20",
+    duration = "1h 4m",
+    releaseDate = "JULY 14",
+    isDownloaded = false,
+    isPlayed = false
+)
--- app/src/main/java/com/mb/jam/ui/EpisodeCard.kt
+++ app/src/main/java/com/mb/jam/ui/EpisodeCard.kt
@@ -5,10 +5,11 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Pause
 import androidx.compose.material.icons.filled.PlayArrow
@@ -23,6 +24,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.mb.jam.model.Episode
@@ -47,24 +49,18 @@ internal fun EpisodeCard(episode: Episode) {
     AnimatedVisibility(visible = uiState.isVisible) {
         Surface(
             modifier = Modifier
-                .fillMaxWidth()
-                .height(64.dp),
+                .fillMaxWidth().wrapContentHeight(),
             color = MaterialTheme.colorScheme.background
         ) {
-            Row {
+            Row(modifier = Modifier.padding(8.dp), verticalAlignment = Alignment.CenterVertically) {
                 Column(
                     modifier = Modifier
-                        .weight(1f)
-                        .fillMaxHeight()
-                        .padding(start = 8.dp),
+                        .weight(1f),
                     verticalArrangement = Arrangement.Center
                 ) {
                     EpisodeCardText(episode)
                 }
                 Column(
-                    modifier = Modifier
-                        .fillMaxHeight()
-                        .padding(16.dp),
                     horizontalAlignment = Alignment.End,
                     verticalArrangement = Arrangement.Center
                 ) {
@@ -84,9 +80,14 @@ private fun EpisodeCardText(episode: Episode) {
         text = "${episode.episodeNumber} - ${episode.releaseDate}",
         style = MaterialTheme.typography.labelMedium
     )
+    Spacer(modifier = Modifier.height(4.dp))
     Text(
-        text = episode.title, style = MaterialTheme.typography.titleMedium
+        text = episode.title,
+        style = MaterialTheme.typography.titleMedium,
+        maxLines = 2,
+        overflow = TextOverflow.Ellipsis
     )
+    Spacer(modifier = Modifier.height(4.dp))
     Text(
         text = episode.duration,
         style = MaterialTheme.typography.labelMedium
@@ -120,6 +121,10 @@ private class EpisodeCardState {
 
     fun togglePlayPauseButton(currentState: Boolean) {
         isPlaying = !currentState
+    }
+
+    fun toggleVisibility(currentState: Boolean) {
+        isVisible = !currentState
     }
 }
 
